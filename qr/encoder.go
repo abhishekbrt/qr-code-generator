767586b5e675671b6f8486fcd6fcedf885ffe7e3
abhishekbrt <abhishekbharti.yz@gmail.com>
2024-09-05 02:26:46 +0530

    Refactor to use strings.Builder for efficient string concatenation

--- qr/encoder.go
+++ qr/encoder.go
@@ -5,41 +5,47 @@ import (
 	"strings"
 )
 
-// for demo purpose we are using error correction mode as Q and version 7 which 45*45 matrix
+// for demo purpose we are using error correction mode as Q and version 7 which is 45*45 matrix
 func Encoder(inputText, errorCorrectionMode string) string {
 	//for byte mode, mode indicator is 0100
 	modeIndicator := "0100"
 	inputTextLength := len(inputText)
 	// for version 1-9, 8 bits are required to store the input character length in byte mode
 	binaryInputTextLength := fmt.Sprintf("%08b", inputTextLength)
-
-	encodedText := modeIndicator + binaryInputTextLength
+	var encodedText strings.Builder
+	encodedText.WriteString(modeIndicator)
+	encodedText.WriteString(binaryInputTextLength)
 	for i := 0; i < inputTextLength; i++ {
-		encodedText += fmt.Sprintf("%08b", inputText[i])
+		encodedText.WriteString(fmt.Sprintf("%08b", inputText[i]))
 	}
-	if len(encodedText) > 704 {
+	if (encodedText.Len()) > 704 {
 		return "input text is too long!"
 	}
 	//for 7-Q version total of 88 bytes means 88*8=704 bits are required
 	//adding 4 bit terminator (0000) to the end
 
-	paddingLength := 704 - len(encodedText)
+	paddingLength := 704 - (encodedText.Len())
 	if paddingLength >= 4 {
-		encodedText += "0000"
+		encodedText.WriteString("0000")
 	} else {
-		encodedText += strings.Repeat("0", paddingLength)
+		encodedText.WriteString(strings.Repeat("0", paddingLength))
 	}
 	//adding 0 as pad bytes for making encodedText multiple of 8
-	encodedText += strings.Repeat("0", (len(encodedText) % 8))
+	encodedText.WriteString(strings.Repeat("0", ((encodedText.Len()) % 8)))
 
 	//adding 11101100 00010001 untill encodedText reach upto 704 bits
-	paddingLen := (704 - len(encodedText)) / 16
-	encodedText += strings.Repeat("1110110000010001", paddingLen)
+	for encodedText.Len() != 704 {
+		encodedText.WriteString("11101100")
+		if encodedText.Len() == 704 {
+			break
+		}
+		encodedText.WriteString("00010001")
+	}
 
 	fmt.Println(inputTextLength)
-	fmt.Printf("%d\n", len(encodedText))
+	fmt.Printf("%d\n", (encodedText.Len()))
 	fmt.Println(inputText)
 	fmt.Println(errorCorrectionMode)
-	return encodedText
+	return encodedText.String()
 
 }
